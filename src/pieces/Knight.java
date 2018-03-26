a810efe691f1cf3baa13f1e29e3edb9746028221
Yael Goldin <ygoldin@hotmail.com>
2018-03-26 15:16:15 -0700

    finding legal knight moves

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -1,5 +1,6 @@
 package pieces;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import gamesetup.*;
@@ -34,7 +35,28 @@ public class Knight implements ChessPiece {
 
 	@Override
 	public Set<PieceMove> legalMoves(ChessBoard board) {
-		// TODO Auto-generated method stub
-		return null;
+		Set<PieceMove> moves = new HashSet<>();
+		int[] firstDirection = new int[] {1, -1};
+		int[] secondDirection = new int[] {2, -2};
+		Integer[] myLocation = board.getSpotOfPiece(this);
+		int myRow = myLocation[0];
+		int myCol = myLocation[1];
+		for(int direction1 : firstDirection) {
+			for(int direction2 : secondDirection) {
+				checkMove(myRow + direction1, myCol + direction2, board, moves);
+				checkMove(myRow + direction2, myCol + direction1, board, moves);
+			}
+		}
+		return moves;
+	}
+	
+	//checks if the knight can move to that spot and what it might take
+	private void checkMove(int curRow, int curCol, ChessBoard board, Set<PieceMove> moves) {
+		if(board.isInBounds(curRow, curCol)) {
+			ChessPiece otherPiece = board.getPieceAtSpot(curRow, curCol);
+			if(otherPiece == null || otherPiece.isWhite() != isWhite) {
+				moves.add(new PieceMove(curRow, curCol, otherPiece));
+			}
+		}
 	}
 }
