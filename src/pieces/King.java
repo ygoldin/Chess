efd320508a90c592ebbfc8a50948a649ddcf9836
Yael Goldin <ygoldin@hotmail.com>
2018-03-26 16:20:31 -0700

    king moves now consider not going into check

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -1,6 +1,7 @@
 package pieces;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import gamesetup.*;
@@ -39,10 +40,13 @@ public class King implements ChessPiece {
 		Integer[] myLocation = board.getSpotOfPiece(this);
 		int myRow = myLocation[0];
 		int myCol = myLocation[1];
+		Map<Integer, Set<Integer>> illegalMoves = movesIntoALineOfFire(board);
 		for(int curRow = myRow - 1; curRow <= myRow + 1; curRow++) {
 			for(int curCol = myCol - 1; curCol <= myCol + 1; curCol++) {
-				if((curRow != myRow || curCol != myCol) && board.isInBounds(curRow, curCol)) {
-					//TODO: not checking for being in check
+				//first check for not staying still, going out of bounds, or going into a line of fire
+				if((curRow != myRow || curCol != myCol) && board.isInBounds(curRow, curCol) &&
+						(!illegalMoves.containsKey(curRow) ||
+						!illegalMoves.get(curRow).contains(curCol))) {
 					ChessPiece otherPiece = board.getPieceAtSpot(curRow, curCol);
 					if(otherPiece == null || otherPiece.isWhite() != isWhite) {
 						moves.add(new PieceMove(curRow, curCol, otherPiece));
@@ -51,5 +55,11 @@ public class King implements ChessPiece {
 			}
 		}
 		return moves;
+	}
+	
+	//returns a map from rows to columns of moves that would put the king in a line of fire
+	private Map<Integer, Set<Integer>> movesIntoALineOfFire(ChessBoard board) {
+		Map<ChessPiece, Integer[]> otherTeam = board.getAllPieces(!isWhite);
+		return null;
 	}
 }
