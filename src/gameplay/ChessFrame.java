452d80740095c441a4e5cbcb14d0dd2257661a4e
Yael Goldin <ygoldin@uw.edu>
2018-03-29 14:13:17 -0700

    more gui setup

--- src/gameplay/ChessFrame.java
+++ src/gameplay/ChessFrame.java
@@ -1,15 +1,47 @@
 package gameplay;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.util.Scanner;
 import javax.swing.*;
 import gamesetup.ChessBoard;
 
+@SuppressWarnings("serial")
 public class ChessFrame extends JFrame {
 	private ChessBoard chessBoard;
 	private Scanner input;
+	private ChessSpot[][] chessSpots;
 	
 	public ChessFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setMinimumSize(new Dimension(1024, 768));
+		setTitle("Chess");
+		
 		input = new Scanner(System.in);
 		chessBoard = new ChessBoard(input);
+		chessSpots = new ChessSpot[ChessBoard.SIZE][ChessBoard.SIZE];
+		JPanel spots = new JPanel();
+		spots.setLayout(new GridLayout(ChessBoard.SIZE, ChessBoard.SIZE));
+		for(int r = 0; r < ChessBoard.SIZE; r++) {
+			for(int c = 0; c < ChessBoard.SIZE; c++) {
+				chessSpots[r][c] = new ChessSpot(r, c);
+				spots.add(chessSpots[r][c]);
+			}
+		}
+		add(spots);
+	}
+	
+	private class ChessSpot extends JButton {
+		private final Color WHITE_SQUARE = Color.WHITE;
+		private final Color BLACK_SQUARE = Color.BLACK;
+		
+		public ChessSpot(int row, int col) {
+			if((row + col) % 2 == 0) {
+				setBackground(WHITE_SQUARE);
+			} else {
+				setBackground(BLACK_SQUARE);
+			}
+		}
 	}
 }
--- src/gameplay/PlayChess.java
+++ src/gameplay/PlayChess.java
@@ -1,5 +1,6 @@
 package gameplay;
 
+import java.awt.EventQueue;
 import java.util.Scanner;
 
 import gamesetup.*;
@@ -8,22 +9,30 @@ import pieces.ChessPiece;
 public class PlayChess {
 
 	public static void main(String[] args) {
-		ChessBoard chessBoard = new ChessBoard();
-		System.out.println(chessBoard);
-		System.out.println();
-		Scanner input = new Scanner(System.in);
-		while(!chessBoard.isGameOver()) {
-			System.out.print("Where do you want to move a piece from? ");
-			int row = input.nextInt();
-			int col = input.nextInt();
-			ChessPiece piece = chessBoard.getPieceAtSpot(row, col);
-			System.out.print("Where do you want to move it to? ");
-			row = input.nextInt();
-			col = input.nextInt();
-			chessBoard.makeMove(piece, row, col);
-			System.out.println(chessBoard);
-		}
-		input.close();
+//		Scanner input = new Scanner(System.in);
+//		ChessBoard chessBoard = new ChessBoard(input);
+//		System.out.println(chessBoard);
+//		System.out.println();
+//		while(!chessBoard.isGameOver()) {
+//			System.out.print("Where do you want to move a piece from? ");
+//			int row = input.nextInt();
+//			int col = input.nextInt();
+//			ChessPiece piece = chessBoard.getPieceAtSpot(row, col);
+//			System.out.print("Where do you want to move it to? ");
+//			row = input.nextInt();
+//			col = input.nextInt();
+//			chessBoard.makeMove(piece, row, col);
+//			System.out.println(chessBoard);
+//		}
+//		input.close();
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				ChessFrame chess = new ChessFrame();
+				chess.pack();
+				chess.setVisible(true);
+			}
+		});
 	}
 
 }
